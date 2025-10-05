c21183231d7349a685893b13d9a388e43f38b5ea
aamya2003 <ahmadasaadmajed1@gmail.com>
2025-10-05 19:55:22 +0300

    first coomit

---
+++ myeasyTools/__init__.py
@@ -0,0 +1,8 @@
+from . import calc, anytools
+
+
+class Empty:
+    def empty():
+        return ''
+
+ModuleName= 'myeasyTools'
---
+++ myeasyTools/calc.py
@@ -0,0 +1,6 @@
+class Calc:
+    def add(x, y):
+        return x +y
+    
+    def subtract(x, y):
+        return x - y
